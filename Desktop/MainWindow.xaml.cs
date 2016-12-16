52ef213f0e08e65337f0a2fc5b50858e34f62d5a
蒲阳 <puyang.c@foxmail.com>
2016-12-16 14:21:55 +0800

    Create New Project

---
+++ Desktop/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using VolunteerDatabase.Entity;
+
+namespace VolunteerDatabase.Desktop
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void createDatabase_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new Database())
+            {
+                if (db.Database.Exists())
+                {
+                    db.Database.Delete();
+                }
+                db.Database.Create();
+            }
+            MessageBox.Show("success");
+        }
+    }
+}
