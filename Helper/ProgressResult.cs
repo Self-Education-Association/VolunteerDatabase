8de6f0a73fbd59e8a44b43ba4fa0a9692920eeb0
CHsan <CHsan@DESKTOP-K7J17DD>
2017-02-03 18:48:59 +0800

    details input

---
+++ Helper/ProgressResult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolunteerDatabase.Helper
+{
+    public class ProgressResult
+    {
+        private string[] _errors;
+
+        private bool _succeeded;
+
+        public string[] Errors { get { return _errors; } }
+
+        public bool Succeeded { get { return _succeeded; } }
+
+        public static ProgressResult Error(params string[] errors)
+        {
+            if (errors.Count() == 0)
+            {
+                errors = new string[] { "未提供错误信息。" };
+            }
+            var result = new ProgressResult
+            {
+                _succeeded = false,
+                _errors = errors
+            };
+            return result;
+        }
+
+        public static ProgressResult Success()
+        {
+            var result = new ProgressResult
+            {
+                _succeeded = true,
+                _errors = { }
+            };
+            return result;
+        }
+
+        private ProgressResult() { }
+    }
+}
