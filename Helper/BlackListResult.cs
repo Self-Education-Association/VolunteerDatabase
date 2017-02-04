2fb86cc71d7653eecef8ce301a89a646fa35b505
Tevain <Tevain@163.com>
2017-02-04 16:13:12 +0800

    黑名单模块报错添加eunm部分

--- Helper/BlackListResult.cs
+++ Helper/BlackListResult.cs
@@ -10,9 +10,10 @@ namespace VolunteerDatabase.Helper
     {
         private bool _succeed = false;
         private string[] _errors;
+        private int _errorvolunteerid;
         public bool Succeed { get { return _succeed; } }
         public string[] Errors { get { return _errors; } }
-        
+        public int ErrorVolunteerId { get { return _errorvolunteerid; } }
         public static BlackListResult Success()
         {
             var result = new BlackListResult
@@ -37,5 +38,73 @@ namespace VolunteerDatabase.Helper
             };
             return result;
         }
+        public static BlackListResult Error(AddBlackListRecordErrorEnum err)
+        {
+            BlackListResult result;
+            switch (err)
+            {
+                case (AddBlackListRecordErrorEnum.NullRecord):
+                    result = Error("欲添加的记录为空，请重新输入。");
+                    break;
+                case (AddBlackListRecordErrorEnum.ExistingRecord):
+                    result = Error("已存在相同记录，请勿重复添加。");
+                    break;
+                case (AddBlackListRecordErrorEnum.WrongTime):
+                    result= Error("填写时间不正确，请检查并稍后重试！");
+                    break;
+                default:
+                    result = Error();
+                    break;
+            }
+            return result;
+        }
+        public static BlackListResult Error(EditBlackListRecordErrorEnum err)
+        {
+          BlackListResult result;
+            switch (err)
+            {
+                case (EditBlackListRecordErrorEnum.EmptyId):
+                    result = Error("请输入Id！");
+                    break;
+                case (EditBlackListRecordErrorEnum.NoExistingRecord):
+                    result = Error("欲编辑的志愿者记录不存在，请重新查询再试。");
+                    break;
+                default:
+                    result = Error();
+                    break;
+            }
+            return result;
+        }
+        public static BlackListResult Error(DeleteBlackListRecordErrorEnum err)
+        {
+            BlackListResult result;
+            switch (err)
+            {
+                case (DeleteBlackListRecordErrorEnum.NonExistingRecord):
+                    result = Error("欲删除的记录不存在！");
+                    break;
+                default:
+                    result = Error();
+                    break;
+            }
+            return result;
+        }
+        #region 枚举类型的错误信息
+        public enum AddBlackListRecordErrorEnum
+        {
+            NullRecord,
+            ExistingRecord,
+            WrongTime
+        }
+        public enum EditBlackListRecordErrorEnum
+        {
+            EmptyId,
+            NoExistingRecord
+        }
+        public enum DeleteBlackListRecordErrorEnum
+        {
+            NonExistingRecord
+        }
+        #endregion
     }
 }
