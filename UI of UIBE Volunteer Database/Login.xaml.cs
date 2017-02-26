b775d4f47073db2909dabd3fee961fdfcd681584
CHsan <CHsan@DESKTOP-K7J17DD>
2017-02-26 21:39:08 +0800

    merge

---
+++ UI of UIBE Volunteer Database/Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void register_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow Window2 = new MainWindow();
+            Application.Current.MainWindow = Window2;
+            Window2.Show();
+            this.Hide();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
