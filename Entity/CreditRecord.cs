5ef2279ac2c32868ce8b07e5bf42def788b83956
aerialmx <aerialmx@gmail.com>
2017-03-03 00:22:56 +0800

    执行对应的数据库迁移和更新，增加查看下属功能

--- Entity/CreditRecord.cs
+++ Entity/CreditRecord.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace VolunteerDatabase.Entity
 {
     public class CreditRecord
     {
+        [Key]
         public Guid UID { get; set; }
         public Volunteer Participant { get; set; }
         public Project Project { get; set; }
