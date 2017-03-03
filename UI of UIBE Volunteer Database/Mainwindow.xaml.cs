cedaafd992514d56f6f821371f883917a69351b0
CHsan <CHsan@DESKTOP-K7J17DD>
2017-03-03 22:19:56 +0800

    更改前端信息显示

--- UI of UIBE Volunteer Database/Mainwindow.xaml.cs
+++ UI of UIBE Volunteer Database/Mainwindow.xaml.cs
@@ -36,10 +36,14 @@ namespace WpfApplication1
         }
         private void ShowUserMessage()
         {
+            string[] orgstring = Claims.User.Organization.OrganizationEnum.ToString().Split('.');
+            string orgdetail = orgstring.Last();
+            string[] rolestring = Claims.Roles.ToString().Split('.');
+            string roledetail = rolestring.Last();
             account_name.Text = Claims.User.AccountName;
-            org.Text = Claims.User.Organization.ToString();
+            org.Text = orgdetail;
             tel.Text = Claims.User.Mobile;
-            roles.Text = Claims.Roles.ToString();
+            roles.Text = roledetail;
             userid.Text = Claims.User.StudentNum.ToString();
             email.Text = Claims.User.Email;
             dormitary.Text = Claims.User.Room;
