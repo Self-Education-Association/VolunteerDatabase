6d053992cae5f729a1185853f51f6f3abf726908
王鹏翔 <@WENPENS-PC>
2017-03-05 22:55:16 +0800

    加入了临时的注册界面

--- UI of UIBE Volunteer Database/Register.xaml.cs
+++ UI of UIBE Volunteer Database/Register.xaml.cs
@@ -20,7 +20,7 @@ namespace Desktop
     /// <summary>
     /// Register.xaml 的交互逻辑
     /// </summary>
-    public partial class Register : UserControl
+    public partial class Register : Window
     {
         public Register()
         {
