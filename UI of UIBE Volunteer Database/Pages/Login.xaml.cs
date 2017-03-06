4f0c6ac58099b272b0944258280a8590df9ece44
王鹏翔 <@WENPENS-PC>
2017-03-06 14:54:49 +0800

    美化了登录注册页面，解决了一些小问题。

--- UI of UIBE Volunteer Database/Pages/Login.xaml.cs
+++ UI of UIBE Volunteer Database/Pages/Login.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using VolunteerDatabase.Helper;
+using FirstFloor.ModernUI.Windows.Controls;
 
 namespace Desktop.Pages
 {
