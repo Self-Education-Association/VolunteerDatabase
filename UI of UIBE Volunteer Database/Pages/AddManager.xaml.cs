592387c570571b7932eeecf4c910ef7d8d8e3d08
aerialmx <aerialmx@gmail.com>
2017-03-08 06:06:03 +0800

    前端完善添加管理员功能

--- UI of UIBE Volunteer Database/Pages/AddManager.xaml.cs
+++ UI of UIBE Volunteer Database/Pages/AddManager.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using VolunteerDatabase.Entity;
+using VolunteerDatabase.Helper;
 
 namespace Desktop.Pages
 {
@@ -20,11 +21,35 @@ namespace Desktop.Pages
     /// </summary>
     public partial class AddManager : Window
     {
+        private IdentityPage identitypage = IdentityPage.GetInstance();
+        AppUserIdentityClaims Claims { get; set; }
+
+        ProjectManageHelper helper;
+
+        IdentityHelper identityhelper;
+
         Project sourceproject;
+
+        List<AppUser> availableuserlist = new List<AppUser>();
         public AddManager(Project project)
         {
             sourceproject = project;
+            helper = ProjectManageHelper.GetInstance();
+            identityhelper = IdentityHelper.GetInstance();
+            Claims = identitypage.Claims;
+            availableuserlist = identityhelper.GetUnderlingsList(Claims.User);
             InitializeComponent();
+            availableUsers.ItemsSource = availableuserlist;
         }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach ( item in collection)
+            {
+
+            }
+        }
+
+
     }
 }
