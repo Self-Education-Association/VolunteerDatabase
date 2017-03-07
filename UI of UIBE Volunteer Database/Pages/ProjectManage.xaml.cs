8f78421124167db1a9009bc397bd79c2c5d167bd
王鹏翔 <@WENPENS-PC>
2017-03-08 02:05:08 +0800

    加入了在项目中添加管理者和志愿者的功能。

--- UI of UIBE Volunteer Database/Pages/ProjectManage.xaml.cs
+++ UI of UIBE Volunteer Database/Pages/ProjectManage.xaml.cs
@@ -151,5 +151,11 @@ namespace Desktop.Pages
             var AddManager = new AddManager();
             AddManager.Show();
         }
+
+        private void AddVolunteer_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var AddVolunteer = new AddVolunteer();
+            AddVolunteer.Show();
+        }
     }
 }
