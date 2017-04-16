d27078f3c424390a54faab3b90aa568bd988a659
Dave Garrison <aerialmx@gmail.com>
2017-04-16 13:11:50 +0800

    修改密码功能前端部分（未完成）

--- UI of UIBE Volunteer Database/Pages/ChangePassWord.xaml.cs
+++ UI of UIBE Volunteer Database/Pages/ChangePassWord.xaml.cs
@@ -41,24 +41,7 @@ namespace VolunteerDatabase.Desktop.Pages
             }
             else
             {
-                var claims = await ih.CreateClaimsAsync(userid.Text, password.Password.ToString());//输入合法性验证
-                if (claims.IsAuthenticated && claims.User.Status == AppUserStatus.Enabled)
-                {
-                    _claimsStored = claims;
-                    SendClaimsEvent?.Invoke(claims);
-                    Close();
-                    ModernDialog.ShowMessage("登陆成功", " ", MessageBoxButton.OK);
-                }
-                else if (claims.User != null && claims.User.Status == AppUserStatus.NotApproved)
-                {
-                    ModernDialog.ShowMessage("已发送用户注册审批请求,请等待机构管理员审批", "注册成功", MessageBoxButton.OK);
-                }
-                else
-                {
-                    Hide();
-                    ModernDialog.ShowMessage("用户名或密码出错，或未通过管理员审批！", "登录失败", MessageBoxButton.OK);
-                    Show();
-                }
+                
             }
         }
     }
